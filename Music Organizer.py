95b3cd4ff25728e1c19b51c79cd7b08e472555af
AlexPanainte <panainte_alex@yahoo.com>
2023-09-05 11:22:28 +0300

    Final commit

--- Music Organizer.py
+++ Music Organizer.py
@@ -1,8 +1,9 @@
 
 import os,random,shutil
 
-music_folder_path="D:/muzicuta/Muzica"
-new_folder_path="D:/muzicuta/"
+print("Music organizer Program\n-----------------------")
+music_folder_path=(input("Adauga calea unde se regaseste muzica:"))
+new_folder_path=(input("Adauga calea unde se vor afla noile fisiere:"))
 
 def new_folder():
     nume=input("Adaugati numele folderului: ")
@@ -10,37 +11,48 @@ def new_folder():
     os.mkdir(name)
 
 def random_music_list():
+    music_list=[]
     content=os.listdir(music_folder_path)
     random.shuffle(content)
+    music_list.append(content)
     return content
 
-
-def list_99():
-    count=0
-    lista=[]
-    for i in random_music_list():
-        lista.append(i)
-        count+=1
-        if count==99:
-            random.shuffle(lista)
-            return lista
-            
-def rename_songs(path):
+def order_songs(music_folder_path):
     old_names = random_music_list()
     new_names = []
 
-    for number, name_song in enumerate(old_names):
+    for number, name_song in enumerate(old_names,start=1):
         new_name = str(number) + "." + name_song
         new_names.append(new_name)
 
     for i in range(len(old_names)):
-        os.rename(path + old_names[i], path + new_names[i]) 
+        source_path = os.path.join(music_folder_path, old_names[i])
+        destination_path = os.path.join(music_folder_path, new_names[i])
+        os.rename(source_path, destination_path)
 
-def muta():
+order_songs(music_folder_path) 
+
+
+def list_99():
+    lista = random_music_list()
+    random.shuffle(lista)
+    return lista[:99]
+            
+def muta_99():
     nume_folder=input("Introduceti nume folder:")
-    source_folder = "D:/muzicuta/Muzica"
-    destination_folder = f"D:/muzicuta/{nume_folder}"
+    source_folder = music_folder_path
+    destination_folder =new_folder_path+nume_folder
     for file_name in list_99():
+        source_path = os.path.join(source_folder, file_name)
+        destination_path = os.path.join(destination_folder, file_name)
+        shutil.move(source_path, destination_path)
+    print("done")
+
+def muta_toate():
+    nume_folder=input("Introduceti nume folder:")
+    source_folder = music_folder_path
+    destination_folder =new_folder_path+nume_folder
+    for file_name in random_music_list():
         source_path = os.path.join(source_folder, file_name)
         destination_path = os.path.join(destination_folder, file_name)
         shutil.move(source_path, destination_path)
@@ -49,19 +61,32 @@ def muta():
 
 def menu():
     optiune=0
-    while optiune!=3:
-        print("""
-        1.Create new folder
-        2.Move music
-        3.Exit""")
+    while optiune!=5:
+        menu=(
+"""--------------------------------------------  
+    1.Create new folder
+    2.Move all music from folder
+    3.Move only 99 songs in new folder
+    4.Order music from selected folder        
+    5.Exit
+--------------------------------------------""")
+        print(menu)
+        
         optiune=int(input("Alegeti optiune: "))
         if optiune==1:
             new_folder()
             print("Folder creat cu succes")
         if optiune ==2:
-            muta()
-            ramase=len(os.listdir("D:/muzicuta/Muzica"))
+            muta_toate()
+            ramase=len(os.listdir(music_folder_path))
             print(f"Muzica mutata cu succes, au mai ramas {ramase} melodii")
+        if optiune ==3:
+            muta_99()
+            ramase=len(os.listdir(music_folder_path))
+            print(f"Muzica mutata cu succes, au mai ramas {ramase} melodii")
+        if optiune ==4:
+            order_songs()
+            print("Melodii numerotate cu succes")
         
-
-menu()
+if __name__ == "__main__":
+    menu()
